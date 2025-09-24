546f802738a5a9b63b378d452c8999bc849b6d84
badriana400952 <badriana0402@gmail.com>
2025-09-24 15:16:36 +0700

    finis

--- dist/Virtual.js
+++ dist/Virtual.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Virtual = void 0;
 const jsx_runtime_1 = require("react/jsx-runtime");
 // Base renderer
-const VirtualBase = ({ name, children, }) => ((0, jsx_runtime_1.jsxs)("section", { "data-virtual": name.toLowerCase(), className: "border-dashed border-2 border-gray-400 rounded p-3 my-2", children: [(0, jsx_runtime_1.jsx)("h3", { className: "text-gray-600", children: name }), children] }));
+const VirtualBase = ({ name, children, }) => ((0, jsx_runtime_1.jsx)("section", { "data-virtual": name.toLowerCase(), children: children }));
 // Proxy + type assertion untuk TypeScript
 exports.Virtual = new Proxy({}, {
     get: (_, prop) => {
--- src/Virtual.tsx
+++ src/Virtual.tsx
@@ -2,29 +2,27 @@ import React from "react";
 
 // Base renderer
 const VirtualBase: React.FC<{ name: string; children: React.ReactNode }> = ({
-  name,
-  children,
+    name,
+    children,
 }) => (
-  <section
-    data-virtual={name.toLowerCase()}
-    className="border-dashed border-2 border-gray-400 rounded p-3 my-2"
-  >
-    <h3 className="text-gray-600">{name}</h3>
-    {children}
-  </section>
+    <section
+        data-virtual={name.toLowerCase()}
+    >
+        {children}
+    </section>
 );
 
 // Proxy + type assertion untuk TypeScript
 export const Virtual: Record<
-  string,
-  React.FC<{ children: React.ReactNode }>
+    string,
+    React.FC<{ children: React.ReactNode }>
 > = new Proxy(
-  {},
-  {
-    get: (_, prop: string) => {
-      return ({ children }: { children: React.ReactNode }) => (
-        <VirtualBase name={prop}>{children}</VirtualBase>
-      );
-    },
-  }
+    {},
+    {
+        get: (_, prop: string) => {
+            return ({ children }: { children: React.ReactNode }) => (
+                <VirtualBase name={prop}>{children}</VirtualBase>
+            );
+        },
+    }
 );
